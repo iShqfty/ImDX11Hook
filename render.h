a2f38eb4ec515516bb3d78ccc5aa4a74db7a6b54
iShqfty <76799463+iShqfty@users.noreply.github.com>
2023-12-16 20:21:17 -0500

    Update render.h
    added resize handling (.h)

--- render.h
+++ render.h
@@ -14,6 +14,7 @@
 #include "hook/include/MinHook.h"
 
 typedef HRESULT(__stdcall* Present) (IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
+typedef HRESULT(__stdcall* ResizeBuffers)(IDXGISwapChain* pSwapChain, UINT BufferCount, UINT Width, UINT Height, DXGI_FORMAT NewFormat, UINT SwapChainFlags);
 typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
 
 namespace rdr {
@@ -23,5 +24,6 @@ namespace rdr {
 
 	LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 	HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
+	HRESULT hkResizeBuffers(IDXGISwapChain* pSwapChain, UINT BufferCount, UINT Width, UINT Height, DXGI_FORMAT NewFormat, UINT SwapChainFlags)
 }
 
